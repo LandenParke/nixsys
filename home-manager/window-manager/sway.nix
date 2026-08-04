e086a70fffe30e7e0ed6bc24d4bf308db8574174
Landen Parke <eddisonparke@gmail.com>
2026-08-04 18:17:12 -0400

    Seperated sway config from main home.nix

---
+++ home-manager/window-manager/sway.nix
@@ -0,0 +1,10 @@
+{
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      terminal = "kitty";
+      defaultWorkspace = "workspace number 1";
+    };
+  };
+}
