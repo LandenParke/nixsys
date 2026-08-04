a7258190f9ffc3748517da9371ce17734dbe51f6
Landen Parke <eddisonparke@gmail.com>
2026-08-04 12:53:52 -0400

    Initial git commit.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # Official NixOS package source, 26.05 branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    helium = {
+      url = "github:AlvaroParker/helium-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{self, nixpkgs, home-manager, ...}: {
+    nixosConfigurations.mouse = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+	home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.landen = import ./home.nix;
+	}
+      ];
+    };
+  };
+}
