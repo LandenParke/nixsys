660326363d8bf3defe20034e7ffb025c70e89018
Landen Parke <eddisonparke@gmail.com>
2026-08-05 10:25:17 -0400

    Added fwupdgmr for updating device firmwares and stuff.

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,8 @@
 
   hardware.graphics.enable = true;
 
+  services.fwupd.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
