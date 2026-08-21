81ab352f3bb7dc90d1bb03608475d2fc47110488
Landen Parke <eddisonparke@gmail.com>
2026-08-20 22:23:44 -0400

    Did a little work on my waybar

--- home-manager/window-manager/waybar.nix
+++ home-manager/window-manager/waybar.nix
@@ -5,9 +5,44 @@
     settings.main = {
       modules-left = [ "sway/workspaces" ];
       modules-center = [ "clock" ];
-      modules-right = [ "cpu" "temperature" "memory" "battery" 
+      modules-right = [ "cpu" "temperature" "memory" "battery"];
+
+      "sway/workspaces" = {
+	disable-scroll = true;
+      };
+
       
-      ];
+      "cpu" = {
+	format = "CPU:{usage}%, ";
+      };
+     "temperature" = {
+	format = "{temperatureC}°C | ";
+      };
+      "memory" = {
+	format = "RAM:{}% | ";
+      };
+      "battery" = {
+	format = "{capacity}%";
+      };
+
     };
+    style = "
+      * {
+	font-family: IBM Plex Mono;
+      }
+      #workspaces button {
+	padding: 0 5px;
+	background-color: transparent;
+      }
+      #workspaces button.focused, #workspaces button.active {
+	background-color: #232323;
+      }
+      #workspaces button.urget {
+	background-color: #710000;
+      }
+     
+
+
+    "; 
   };
 }
